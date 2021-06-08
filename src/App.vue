c04030010fa08248ca2bc6ce0084e064292c86a9
sebascasasg <sebascasas.developer@gmail.com>
2021-06-07 22:55:44 -0500

    Import global styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <start-view />
+  </div>
 </template>
 
 <script>
-export default {}
+import StartView from '@/components/StartView.vue'
+export default {
+  components: {
+    StartView,
+  },
+}
 </script>
 
 <style scoped></style>
